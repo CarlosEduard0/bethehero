0f34207a9c19f451add2e47561bf61451d6c5cda
Carlos Eduardo <carloseduardofox8@gmail.com>
2020-04-01 00:23:37 +0000

    feat: adiciona express e altera dockerfile

---
+++ frontend/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+const baseDir = `${__dirname}/build`
+
+app.use(express.static(baseDir));
+
+app.get('*', (request, response) => {
+    response.sendFile('index.html', { root: baseDir })
+});
+
+const port = ;
+
+app.listen(process.env.PORT || 3000);
